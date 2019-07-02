6766b4e350e0676c024145d74188915f2a0aeaf4
Vineet <vineetdev2008@gmail.com>
2019-07-03 02:18:17 +0530

    Basic querying with prisma binding done

--- graphql-prisma/src/index.js
+++ graphql-prisma/src/index.js
@@ -6,6 +6,7 @@ import Post from "./resolvers/Post";
 import User from "./resolvers/User";
 import Comment from "./resolvers/Comment";
 import Subscription from "./resolvers/Subscription";
+import './prisma';
 
 const pubsub = new PubSub();
 
