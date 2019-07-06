ce36b9982ad8f336dc1b30135a50fc5fd49fea0a
Vineet <vineetdev2008@gmail.com>
2019-07-06 20:21:42 +0530

    local postgres set

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -1,1 +1,30 @@
+
 import {Prisma} from "prisma-binding";
+
+const prisma = new Prisma({
+    typeDefs: 'src/generated/prisma.graphql',
+    endpoint: 'http://localhost:4466'
+});
+
+const updatePostForUser = async (postId, data) => {
+    const isPostExist = await prisma.exists.Post({id: postId});
+
+    if(!isPostExist) {
+        throw new Error(`Post with id ${postId} doesn't exist`);
+    }
+
+    const updatedPost = await prisma.mutation.updatePost({
+        where: {
+            id: postId
+        },
+        data
+    }, '{ author { name posts { title body published } } }' );
+
+    return updatedPost.author;
+}
+
+updatePostForUser("5d1a2ab70274390003387769", {
+    title: "First update to first post",
+    body: "The first updation is awesome",
+    published: false
+}).then(user => console.log(JSON.stringify(user, null, 2))).catch(e => console.error(e))
