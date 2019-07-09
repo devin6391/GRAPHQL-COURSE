3ab02e8499d23b2870547845369b07a08c342aea
Vineet <vineetdev2008@gmail.com>
2019-07-09 23:36:40 +0530

    most of queries except users locked

--- graphql-prisma/src/resolvers/Query.js
+++ graphql-prisma/src/resolvers/Query.js
@@ -1,3 +1,5 @@
+import getUserId from '../utils/getUserId';
+
 const Query = {
   users(parent, args, ctx, info) {
     const {prisma} = ctx;
@@ -19,19 +21,42 @@ const Query = {
   posts(parent, args, ctx, info) {
     const {prisma} = ctx;
     const {query} = args;
-    const opArgs = {};
+    const opArgs = {
+      where: {
+        published: true
+      }
+    };
 
     if(query) {
-      opArgs.where = {
-        OR: [{
+      opArgs.where.OR = [{
           title_contains: query
         }, {
           body_contains: query
         }]
-      }
     }
 
     return prisma.query.posts(opArgs, info);
+  },
+  myPosts(parent, args, ctx, info) {
+    const userId = getUserId(ctx.request);
+    const {query} = args;
+    const opArgs = {
+      where: {
+        author: {
+          id: userId
+        }
+      }
+    };
+
+    if(query) {
+      opArgs.where.OR = [{
+          title_contains: query
+        }, {
+          body_contains: query
+        }]
+    }
+
+    return ctx.prisma.query.posts(opArgs, info);
   },
   comments(parent, args, ctx, info) {
     const {prisma} = ctx;
@@ -45,6 +70,36 @@ const Query = {
     }
 
     return prisma.query.comments(opArgs, info);
+  },
+  async post(parent, args, ctx, info) {
+    const userId = getUserId(ctx.request, false);
+
+    const posts = await ctx.prisma.query.posts({
+      where: {
+        id: args.id,
+        OR: [{
+          published: true
+        }, {
+          author: {
+            id: userId
+          }
+        }]
+      }
+    }, info);
+
+    if(posts.length === 0) {
+      throw new Error("Post not found");
+    }
+
+    return posts[0];
+  },
+  me(parent, args, ctx, info) {
+    const userId = getUserId(ctx.request);
+    return ctx.prisma.query.user({
+      where: {
+        id: userId
+      }
+    })
   }
 };
 
