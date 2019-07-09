9dfc054ab9ba3ffe838ef63ab2641f4b92c7a1e7
Vineet <vineetdev2008@gmail.com>
2019-07-10 01:38:46 +0530

    Fragments started and basics done

--- graphql-prisma/src/index.js
+++ graphql-prisma/src/index.js
@@ -1,25 +1,13 @@
 import { GraphQLServer, PubSub } from "graphql-yoga";
 import db from "./db";
-import Query from "./resolvers/Query";
-import Mutation from "./resolvers/Mutation";
-import Post from "./resolvers/Post";
-import User from "./resolvers/User";
-import Comment from "./resolvers/Comment";
-import Subscription from "./resolvers/Subscription";
 import prisma from './prisma';
+import {resolvers, fragmentReplacements} from './resolvers'
 
 const pubsub = new PubSub();
 
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
-  resolvers: {
-    Query,
-    Mutation,
-    Subscription,
-    Post,
-    User,
-    Comment
-  },
+  resolvers,
   context(request) {
     return{
       db,
@@ -27,7 +15,8 @@ const server = new GraphQLServer({
       prisma,
       request
     }
-  }
+  },
+  fragmentReplacements
 });
 
 server.start(() => {
--- graphql-prisma/src/resolvers/User.js
+++ graphql-prisma/src/resolvers/User.js
@@ -1,5 +1,31 @@
+import getUserId from '../utils/getUserId';
+
 const User = {
-  
+  email: {
+    fragment: 'fragment userId on User { id }',
+    resolve(parent, args, ctx, info) {
+        const userId = getUserId(ctx.request, false);
+
+        if(userId && userId === parent.id) {
+          return parent.email;
+        } else {
+            return null;
+        }
+    }
+  },
+  posts: {
+    fragment: 'fragment userId on User { id }',
+    resolve(parent, args, ctx, info) {
+      return ctx.prisma.query.posts({
+        where: {
+          published: true,
+          author: {
+            id: parent.id
+          }
+        }
+      })
+    }
+  }
 };
 
 export default User;
