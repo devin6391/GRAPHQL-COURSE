d6fb0e730f7b56ceb174713f9b8d766b7e6b1fc8
Vineet <vineetdev2008@gmail.com>
2019-07-14 13:34:49 +0530

    Babel packages upgraded

--- graphql-prisma/src/index.js
+++ graphql-prisma/src/index.js
@@ -1,30 +1,5 @@
 import '@babel/polyfill/noConflict';
-import {
-  GraphQLServer,
-  PubSub
-} from "graphql-yoga";
-import db from "./db";
-import prisma from './prisma';
-import {
-  resolvers,
-  fragmentReplacements
-} from './resolvers'
-
-const pubsub = new PubSub();
-
-const server = new GraphQLServer({
-  typeDefs: "./src/schema.graphql",
-  resolvers,
-  context(request) {
-    return {
-      db,
-      pubsub,
-      prisma,
-      request
-    }
-  },
-  fragmentReplacements
-});
+import server from './server';
 
 server.start({
   port: process.env.PORT || 4000
