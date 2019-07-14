bef31d527a069186f068e77f1381d4bc1878fb6b
Vineet <vineetdev2008@gmail.com>
2019-07-14 15:29:39 +0530

    Many babel plugins and settings refurbished

--- graphql-prisma/tests/user.test.js
+++ graphql-prisma/tests/user.test.js
@@ -1,7 +1,30 @@
-test('This is my first test case', () => {
+import '@babel/polyfill';
+import 'cross-fetch/polyfill';
+import ApolloClient, {
+    gql
+} from 'apollo-boost';
 
+const client = new ApolloClient({
+    uri: "http://localhost:4000"
 });
 
-test('This is my second test case', () => {
+test('Should create new user', async () => {
+    const createUser = gql `
+        mutation {
+        createUser(
+            data: { name: "Greg", email: "Greg@example.com", password: "Greg1234" }
+        ) {
+            token
+            user {
+            id
+            }
+        }
+        }
+    `;
+
+    const response = await client.mutate({
+        mutation: createUser
+    });
+
 
 })
